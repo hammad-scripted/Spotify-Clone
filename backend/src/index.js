0e14335201763062a3bc4a1e4b4449bd194eca8d
Mohammad Hammad <hammad.scripted@gmail.com>
2026-06-13 22:57:29 +0530

    authentication middleware setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,18 @@ import express from 'express';
 import chalk from 'chalk';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import { clerkMiddleware } from '@clerk/express';
 import dns from 'node:dns/promises';
-dns.setServers(['8.8.8.8','1.1.1.1']);
-import {connectDB} from './db/connect.js'
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+import { connectDB } from './db/connect.js';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-import userRouter from "./routes/user.route.js"
-import authRouter from "./routes/auth.route.js"
-import adminRouter from "./routes/admin.route.js"
-import statsRouter from "./routes/stats.route.js"
-import songRouter from "./routes/song.route.js"
-import albumRouter from "./routes/album.route.js"
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import adminRouter from './routes/admin.route.js';
+import statsRouter from './routes/stats.route.js';
+import songRouter from './routes/song.route.js';
+import albumRouter from './routes/album.route.js';
 import errorHandler from './errors/errorHandler.js';
 import { notFound } from './errors/notFound.js';
 const app = express();
@@ -21,31 +22,28 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(clerkMiddleware({ secretKey: process.env.CLERK_SECRET_KEY })); //! this is a middleware for authentication in clerk
 
 // * ROUTES
-app.use("/api/v1/users",userRouter)
-app.use("/api/v1/auth",authRouter)
-app.use("/api/v1/songs",songRouter)
-app.use("/api/v1/admin",adminRouter)
-app.use("/api/v1/albums",albumRouter)
-app.use("/api/v1/stats",statsRouter)
-app.use(notFound)
-app.use(errorHandler)
-
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/songs', songRouter);
+app.use('/api/v1/admin', adminRouter);
+app.use('/api/v1/albums', albumRouter);
+app.use('/api/v1/stats', statsRouter);
+app.use(notFound);
+app.use(errorHandler);
 
 // * SERVER
-const startServer=async()=>{
-
-  try{
+const startServer = async () => {
+  try {
     await connectDB();
     app.listen(PORT, () => {
-      console.log(chalk.magenta.italic( `Server is listening on port ${PORT}`));
-    });   
+      console.log(chalk.magenta.italic(`Server is listening on port ${PORT}`));
+    });
+  } catch (error) {
+    console.log(error);
   }
-  catch(error){
-    console.log(error)
-  } 
-}
+};
 
 startServer();
---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import { clerkClient, getUser } from '@clerk/express';
+import { StatusCodes } from 'http-status-codes';
+import ApiError from '../utils/apiError.js';
+export const protectRoute = async (req, res, next) => {
+  if (!req.auth.userId) {
+    throw new ApiError(StatusCodes.UNAUTHORIZED, 'You are not logged in!');
+  }
+  next();
+};
+
+export const checkRole = async (req, res, next) => {
+  try {
+    const currentUser = await clerkClient.users.getUser(req.auth.userId);
+    console.log(currentUser);
+    const isAdmin =
+      process.env.ADMIN_EMAIL === currentUser.emailAddresses[0].emailAddress;
+    if (!isAdmin) {
+      throw new ApiError(StatusCodes.UNAUTHORIZED, 'You are not an admin!');
+    }
+    next();
+  } catch (e) {
+    throw new ApiError(
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      'You are not an admin!',
+    );
+  }
+};
