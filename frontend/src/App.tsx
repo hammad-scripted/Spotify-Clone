65f6f0d2ed0802e1d6504e3855aee768c5917a90
Mohammad Hammad <hammad.scripted@gmail.com>
2026-06-13 13:03:27 +0530

    added clerk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import {Button} from "@/components/ui/button";
-
+ 
 const App = () => {
   return (
     <div className="text-3xl font-bold underline text-red-600">App
-    <Button>Click Me</Button>
+    <Button variant="secondary">Click Me</Button>
     
     </div>
   )
