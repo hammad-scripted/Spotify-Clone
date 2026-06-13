5a24150d17cc7c5c80530043bcdfa1ca56a116e1
Mohammad Hammad <hammad.scripted@gmail.com>
2026-06-13 12:28:19 +0530

    setup shadcn and tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
-
+import {Button} from "@/components/ui/button";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="text-3xl font-bold underline text-red-600">App
+    <Button>Click Me</Button>
+    
+    </div>
   )
 }
 
