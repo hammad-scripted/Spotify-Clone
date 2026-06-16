dec4e63ff20a650aa2a1b21c158c15e8ab9d0389
Mohammad Hammad <hammad.scripted@gmail.com>
2026-06-16 23:10:26 +0530

    user controller and route initiated

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import { User } from '../models/user.model.js';
+import ApiResponse from '../utils/apiResponse.js';
+import ApiError from '../utils/apiError.js';
+import { StatusCodes } from 'http-status-codes';
+
+export const getAllUsers = async (req, res) => {
+  const users = await User.find({});
+
+  return res
+    .status(StatusCodes.OK)
+    .json(new ApiResponse(StatusCodes.OK, users, 'Users fetched successfully'));
+};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,14 @@
-import { Router } from "express";
+import { Router } from 'express';
 import ApiResponse from '../utils/apiResponse.js  ';
+import ApiError from '../utils/apiError.js';
+import { User } from '../models/user.model.js';
+import { protectRoute, checkRole } from '../middlewares/auth.middleware.js';
+import { getAllUsers } from '../controllers/user.controller.js';
+import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 
-router.get("/",(req,res)=>{
-   res.json(new ApiResponse(200,'hello'))   ;
-})
+router.get('/', protectRoute, getAllUsers);
+// Todo get messages
 
 export default router;
