a807d9b0f71a25bd27f0ff3c213eb0abe38cd20b
Mohammad Hammad <hammad.scripted@gmail.com>
2026-06-17 22:58:00 +0530

    feat:stats controller setup

---
+++ backend/src/controllers/stats.controller.js
@@ -0,0 +1,42 @@
+
+
+import { Router } from 'express';
+import ApiResponse from './../utils/apiResponse.js';
+import ApiError from './../utils/apiError.js';
+import { StatusCodes } from 'http-status-codes';
+import { Song } from '../models/song.model.js';
+import { Album } from '../models/album.model.js';
+import { User } from '../models/user.model.js';
+
+export const getStats = async (req, res) => {
+  const { totalSongs, totalAlbums, totalUsers, uniqueArtists } =
+    await Promise.all([
+      Song.countDocuments(),
+      Album.countDocuments(),
+      User.countDocuments(),
+
+      Song.aggregate([
+        {
+          $unionWith: {
+            coll: 'albums',
+            pipeline: [],
+          },
+        },
+        { $group: { _id: '$artist' } },
+        { $count: 'count' },
+      ]),
+    ]);
+
+  return res.status(StatusCodes.OK).json(
+    new ApiResponse(
+      StatusCodes.OK,
+      {
+        totalSongs,
+        totalAlbums,
+        totalUsers,
+        totalArtists: uniqueArtists[0]?.count || 0,
+      },
+      'Stats fetched successfully',
+    ),
+  );
+}
--- backend/src/routes/stats.route.js
+++ backend/src/routes/stats.route.js
@@ -1,44 +1,8 @@
-import { Router } from 'express';
-import ApiResponse from './../utils/apiResponse.js';
-import ApiError from './../utils/apiError.js';
-import { StatusCodes } from 'http-status-codes';
-import { Song } from '../models/song.model.js';
-import { Album } from '../models/album.model.js';
-import { User } from '../models/user.model.js';
+import express from "express";
+import { Router } from "express";
 import {protectRoute,checkRole} from '../middlewares/auth.middleware.js'
-
+import {getStats} from '../controllers/stats.controller.js'
 const router = Router();
 
-router.get('/',protectRoute,checkRole, async (req, res) => {
-  const { totalSongs, totalAlbums, totalUsers, uniqueArtists } =
-    await Promise.all([
-      Song.countDocuments(),
-      Album.countDocuments(),
-      User.countDocuments(),
-
-      Song.aggregate([
-        {
-          $unionWith: {
-            coll: 'albums',
-            pipeline: [],
-          },
-        },
-        { $group: { _id: '$artist' } },
-        { $count: 'count' },
-      ]),
-    ]);
-
-  return res.status(StatusCodes.OK).json(
-    new ApiResponse(
-      StatusCodes.OK,
-      {
-        totalSongs,
-        totalAlbums,
-        totalUsers,
-        totalArtists: uniqueArtists[0]?.count || 0,
-      },
-      'Stats fetched successfully',
-    ),
-  );
-});
+router.get('/',protectRoute,checkRole, getStats);
 export default router;
